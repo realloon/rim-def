ac4ecf498fa1afae956baa6644d7c171c7d516fa
realon <sincerajn@outlook.com>
2024-12-30 18:06:46 +0800

    Adjust export order.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { defineGun, defineBullet, defineSound } from './define'
 import { createPatch } from './patch'
 import { output } from './helper'
 
-export { defineGun, defineBullet, defineSound, output, createPatch }
+export { defineGun, defineBullet, defineSound, createPatch, output }
