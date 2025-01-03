e8d0e5c572b49a55a77f09c7c294aca7229968e6
realon <sincerajn@outlook.com>
2025-01-03 16:35:59 +0800

    Adjust export mode.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { defineGun, defineBullet, defineSound } from './define'
-import { createPatch } from './patch'
+import definePatch from './patch'
+import defineAbout from './about'
+import useI18n from './helper/useI18n'
 import output from './helper/output'
 
-export { defineGun, defineBullet, defineSound, createPatch, output }
+export {
+  defineGun,
+  defineBullet,
+  defineSound,
+  definePatch,
+  defineAbout,
+  useI18n,
+  output,
+}
--- src/patch/index.ts
+++ src/patch/index.ts
@@ -1,8 +1,10 @@
 import Patcher from './Patcher'
 
-export function createPatch(matches?: string | Array<string>) {
+function definePatch(matches?: string | Array<string>) {
   if (!matches) return new Patcher()
 
   matches = Array.isArray(matches) ? matches : [matches]
   return new Patcher(matches)
 }
+
+export default definePatch
