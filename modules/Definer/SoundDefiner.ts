96d05e9fe1a03c4194b5a5810ee9d78ada0ca634
sincerajn <sincerajn@outlook.com>
2025-06-20 10:12:00 +0800

    remove old code.

--- modules/Definer/SoundDefiner.ts
+++
@@ -1,8 +0,0 @@
-import { SoundDefined } from '#defined'
-import Definer from './Definer'
-
-export default class SoundDefiner extends Definer {
-  constructor(defName: string, defined: SoundDefined) {
-    super(defName, defined)
-  }
-}
